5f8ec92b82a696cc0dfc1968992e93a378b6067e
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-14 10:03:12 +0530

    initial commit

---
+++ java-training/src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 1;
+        int b = 2;
+        System.out.println(a+b);
+    }
+}
