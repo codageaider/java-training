21159e45e325e184a92a8bdda51553d8b4e0e3bf
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-16 12:30:54 +0530

    random

--- java-training/src/day3/FileExample.java
+++ java-training/src/day3/FileExample.java
@@ -2,24 +2,42 @@ package day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 /*
 File class
  */
 public class FileExample {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File file = new File("test.txt");
         System.out.println(file.getName());
         System.out.println(file.getAbsolutePath());
 
         File file2 = new File("test2.txt");
-        PrintWriter printWriter = new PrintWriter(file2);
-        printWriter.println("This is a code created file");
-        printWriter.println("This is the second line");
+        // try with resources
+        // Autocloseable
+        try(PrintWriter printWriter = new PrintWriter(file2);) {
+            printWriter.println("This is a code created file");
+            printWriter.println("This is the second line");
+        }
 
-        printWriter.close();
-
+        try(Scanner scanner = new Scanner(file2);) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        }
+        URL url  = new URL("http://www.google.com/");
+        Scanner input = new Scanner(url.openStream());
+        while(input.hasNext()){
+            System.out.println(input.nextLine());
+        }
+        /*
+        read an expression from a file and output it's result
+         */
 
     }
 }
