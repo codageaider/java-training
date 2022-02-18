f68655022830eb5693fe5963a0eab679dfccb3e9
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-18 14:51:52 +0530

    random

--- java-training/src/homework/homework_3/HomeWork.java
+++ java-training/src/homework/homework_3/HomeWork.java
@@ -1,30 +1,17 @@
 package homework.homework_3;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HomeWork {
-}
+    public static void main(String[] args) {
 
-class Song {
-    String title;
-    int playCount;
-    List<String> genre;
-}
-/*
-Suppose you are given a List of songs.
-genre = pop, rock,
-write a function that returns for each genre the song that is played
-the maximum number of times.
-
-song = {"title-1", 5 , ["pop","rock"]}, {"title-2",6,["pop"]}, {"title-3",4,["rock"]}
-output:
-pop -> title-2,6
-rock -> title-1, 5
- */
-
-/*
-
-public static void genreMaxPlayedSong(List<Song> songs) {
+    }
+// genreMaxPlayedSong(List<Song> songs, int k) -> it returns
+    // for each genre the top k played songs
+    public static void genreMaxPlayedSong(List<Song> songs) {
         HashMap<String, Song> map = new HashMap<>();
         for (Song song : songs) {
             List<String> genres = song.getGenre();
@@ -45,5 +32,54 @@ public static void genreMaxPlayedSong(List<Song> songs) {
             System.out.println(entry.getKey() + " -> " + song.getTitle() + "," + song.getPlayCount());
         }
     }
+}
+
+class Song {
+    String title;
+    List<String> genre;
+    int playCount;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+}
+/*
+Suppose you are given a List of songs.
+genre = pop, rock,
+write a function that returns for each genre the song that is played
+the maximum number of times.
+
+song = {"title-1", 5 , ["pop","rock"]}, {"title-2",6,["pop"]}, {"title-3",4,["rock"]},
+{"title-4",4,["pop","rock","classical"]},{"title-5",10,["jazz","pop"],
+{"title-6",1,["jazz"]}, {"title-7",6,["jazz"]}
+}
+output:
+pop -> title-2,6
+rock -> title-1, 5
+}
+k=2
+Map<String, List<>> function(songList, int k){
+output: 
+pop ->  title-2 , 6 | title-5 , 10
+rock -> title-1 ,5 | title-3 , 4
+classical -> title-4 , 4
+jazz -> title-5 , 10 |title-7,  6
+
+
+}
+ */
+
+/*
+
+
 
  */
