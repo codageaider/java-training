8a92e8210bbe476bbe20e45e84ddd9ea66e3d914
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-18 11:57:32 +0530

    random

--- java-training/src/day5/Example.java
+++ java-training/src/day5/Example.java
@@ -9,19 +9,27 @@ public class Example {
             if(i==500)
                 str=null;
         }
+/*
+         after the 500th iteration the str refers to null.
+         But still the string "asdasda" takes up space in RAM
+         Garbage collection
+         It automatically finds out which memory can be cleared.
+         and is no longer referenced
+         Is done using a Daemon thread. A thread that's working in
+         the background.
 
-        // after the 500th iteration the str refers to null.
-        // But still the string "asdasda" takes up space in RAM
-        // Garbage collection
-        // It automatically finds out which memory can be cleared.
-        // and is no longer referenced
-        // Is done using a Daemon thread. A thread that's working in
-        // the background.
+         Garbage collector thread -> is to help support other tasks or the user tasks.
+         (i) background threads
+         (ii) service threads -> provide service to user threads.
+         (iii)
 
-        // Garbage collector thread -> is to help support other tasks or the user tasks.
-        // (i) background threads
-        // (ii) service threads -> provide service to user threads.
-
+        Two types of threads
+        (i) User Thread
+        (ii) Daemon Thread
+               1) will exit if there is no user thread running
+               2) There was an infinite loop in daemon thread, but still the program terminated
+               3) When there was an inifinite loop in a user thread. The program didn't terminate
+*/
         Thread daemonThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -33,14 +41,15 @@ public class Example {
         daemonThread.setDaemon(true);
         daemonThread.start();
 
+
+        // This is a user thread
         new Thread(new Runnable() {
             @Override
             public void run() {
+
             }
         }).start();
 
-        while(true){
-            System.out.println(daemonThread.getName() + " , "+daemonThread.isDaemon() + " , "+ daemonThread.getState());
-        }
+
     }
 }
