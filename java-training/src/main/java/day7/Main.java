3ff61119f605c5e58d8779ecfb42e0b2c66890ad
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-22 10:31:49 +0530

    random

---
+++ java-training/src/main/java/day7/Main.java
@@ -0,0 +1,27 @@
+package day7;
+
+/*
+Hibernate - ORM
+ORM -> Object relational mapping
+Person object, I want to store this person object in a table.
+Person is a class , save this object
+It will automatically insert the person object  into a table.
+
+We define a person class and create an object of it
+hibernate will map this object to the mysql table.
+ORM 
+
+// persistence -> storage
+JPA -> Java persistence API  
+       (Jakarta persistence API) <-- 2021
+       
+JPA is part o java library and it provides a set of interface
+Hibernate provides implementation of those interfaces.
+JPA providers
+Hibernate is one example of JPA provider.
+Another example of JPA provider is EclipseLink
+
+we need to provide it details
+ */
+public class Main {
+}
