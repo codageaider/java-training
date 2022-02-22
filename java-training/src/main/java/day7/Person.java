5707afcc0eabb67dff94e70d4754eb07f63c839a
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-22 11:59:00 +0530

    random

--- java-training/src/main/java/day7/Person.java
+++ java-training/src/main/java/day7/Person.java
@@ -5,6 +5,22 @@ public class Person {
     private String name;
     private String address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     Person(){
 
     }
@@ -12,5 +28,11 @@ public class Person {
         this.id = id;
         this.name = name;
         this.address = address;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 }
