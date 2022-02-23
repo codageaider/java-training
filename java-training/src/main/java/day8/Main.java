6272c939ba810e69a5cb310d23776c07c2088bfd
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-23 12:37:05 +0530

    random

--- java-training/src/main/java/day8/Main.java
+++ java-training/src/main/java/day8/Main.java
@@ -10,6 +10,23 @@ and print out the result.
 Integer id, Double speed, String name, Float horsePower, Character maker,
 Boolean isHatchBack, Long carNumber
 I have included all the primitive data types/wrapper
+
+(v) Create a Student class
+Integer id;
+List<Integer> marks;
+There is direct mapping available between a List and any table column
+example marks = [10, 20, 30]
+Store it as a comma separated string in the table.
+new Student(10,Arrays.asList(10,20,30))
+In the Student table there should be 2 colmsn id and marks
+id-> int
+marks-> varchar
+you need to convert the List<Integer> to a String
+String s = "10, 20,30" and store this string in the table instead of the List<Integer>
+setMarks and getMarks <-- modify these two methods
+setMarks(String marks)  <-- convert String to a list of integer
+String getMarks() -> convert the list to String
+
  */
 public class Main {
 }
