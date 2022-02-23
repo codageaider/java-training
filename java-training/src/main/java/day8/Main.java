6c508e67995741619dd21078e774f947cfd01dc8
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-02-23 10:28:47 +0530

    random

---
+++ java-training/src/main/java/day8/Main.java
@@ -0,0 +1,11 @@
+package day8;
+/*
+(i) Create a new class Student(id,name,rollno,university) and insert some entries into it using
+the increment generator. (4-5)
+(ii) write a separate statement to Query all the records in the Student table and fetch them in a list.
+and print out the result.
+(iii) write your own CustomIdGenerator which generates a random 5 digit number
+
+ */
+public class Main {
+}
