4c1fc164b48069e2fda61471cdcef2644a9d179b
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-05 10:24:15 +0530

    random

--- java-training/src/main/java/day12/Main.java
+++ java-training/src/main/java/day12/Main.java
@@ -8,7 +8,7 @@ Postman <-- This is used to make HTTP GET, POST, PUT method calls
 public class Main {
     public static void main(String[] args) {
         Users user = new Users();
-        user.addSortingMethod(new BubbleSort());
+        user.addSortingMethod(new MergeSort());
         user.sort();
     }
 }
