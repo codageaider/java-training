f2730e55e079c15b881471fcfd0f4e81333f0b1a
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-06 09:41:50 +0530

    random

---
+++ java-training/src/main/java/day13/Main.java
@@ -0,0 +1,15 @@
+package day13;
+
+/*
+(i)write a class Database with a constructor taking in appropriate
+parameters. Include appropriate fields of this class like
+mysql url, username, password, database.
+
+(ii) Make this class Singleton
+
+(iii) Make it Lazy loaded
+
+ */
+public class Main {
+
+}
