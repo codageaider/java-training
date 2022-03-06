cc77052db34d4e61f0a075371f368489823066b0
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-06 17:03:56 +0530

    random

--- java-training/src/main/java/day13/Main.java
+++ java-training/src/main/java/day13/Main.java
@@ -55,5 +55,72 @@ name, address, company, yearOfExperience, Manager, languages
 
 JSON Object starts and ends ith {}
 JSON array looks like []
+Nested Json
 Each element in JSON is a key value pair.
+
+HTTP response status codes
+1xx -> Informational responses
+2xx -> Successful responses
+3xx -> Redirection messages
+4xx -> client error responses
+5xx -> Server error responses
+
+GET, <-- When you want to fetch some information from the server
+POST, <-- When you create some new data at server side
+PUT, <-- Is used to update an existing data at server side
+DELETE <-- When you are deleting a data
+
+Twitter
+
+https://www.twitter.com
+
+1) User can create an account
+https://www.twitter.com/create  POST
+{"name":"","email":""}
+
+2) User can fetch account details
+https://www.twitter.com/fetch?email=tim@gmail.com  GET
+
+3) User can update account details
+https://www.twitter.com/update PUT
+{"name":"","email":}
+
+4) User can delete account
+https://www.twitter.com/delete?email=tim@gmail.com DELETE
+
+Ex:
+  Write API for following
+(i) When the user click on a follow button
+    https://www.twitter.com/follow   PUT/POST
+    {
+    "userID" : "" // the userID of the requester
+    "followUserID" : ""
+    }
+    Database : userID and a list of it's followers
+    So here I am creating a new follower, or
+    I am adding one more follower to the list
+
+
+(ii) A new tweet is posted
+https://www.twitter.com/tweet/new   POST
+{
+"userID":"",
+"tweet":"content of tweet"
+}
+
+(iii) Return all tweets
+https://www.twitter.com/tweet?userId=123   GET
+{"tweets":["tweet1","tweet2","tweet2"]}
+
+Designing HTTP API
+(i) The URL (ii) HTTP Method (iii) Request Body (iv) Response Body (v) Query Parameters
+Ex :
+Think of designing API's for facebook.
+5 API's
+1) User account creationg
+2) Follow another user
+3) Friend another user
+4) unfriend another user
+5) post
+6) likes
  */
