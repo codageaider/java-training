f921924a307be6ac5983fdb7e22e152c1957b41f
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-06 10:57:20 +0530

    random

--- java-training/src/main/java/day13/Main.java
+++ java-training/src/main/java/day13/Main.java
@@ -9,6 +9,20 @@ mysql url, username, password, database.
 
 (iii) Make it Lazy loaded
 
+
+------------------------------------------
+(iv) Achieve the same thing using Java based Configuration ( The @Bean annotation)
+
+(v) Achieve the same thing using the xml based configuration
+
+Bean scope in xml can be specified like this -
+<bean scope="singleton"/>
+<bean scope="prototype"/>
+
+(vi) After configuring the beans with xml. Do test whether you are getting
+the same object or different object by comparing the hashcode.
+
+
  */
 public class Main {
 
