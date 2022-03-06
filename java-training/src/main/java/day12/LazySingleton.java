20935caa6918c0a9a4a186860110b4b29911e5d0
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-06 11:46:30 +0530

    random

--- java-training/src/main/java/day12/LazySingleton.java
+++ java-training/src/main/java/day12/LazySingleton.java
@@ -15,6 +15,19 @@ public class LazySingleton {
     }
 }
 /*
+Thread-1 is at line 12, Thread-2 is at line 12  Both will go to line 13 because the variable is currently null
+Thread-1 goes to line13. lazySingleton=new LazySingleton();
+Thread-1 goes to line 14 and return lazySingleton is executed
+
+Thread-2 is at line 13 and it assigned lazySingleton = new LazySingleton();
+This will cause a new object to be created which is different from the earlier
+
+Thread-1  :    LazySingleton lazySingleton = LazySingleton.getInstance();  <--- This will be assigned an object new LazySingleton()
+Thread-2  :    LazySingleton lazySingleton = LazySingleton.getInstance(); <-- This will get a different new LazySingleton()
+So our class breaks the singleton property in a multithreaded scenario.
+
+ */
+/*
 There are in general two types of Object Initializations /creations
 (i) Lazy Instantion (Lazy Loading/ Lazy Initialization)
 (ii) Eager Instantion (Eager Loading / Eadger Initialization)
