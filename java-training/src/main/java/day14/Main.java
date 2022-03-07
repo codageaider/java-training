1834d7859a937748ed6c78b30046806e885c9233
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-07 09:47:20 +0530

    random

--- java-training/src/main/java/day14/Main.java
+++ java-training/src/main/java/day14/Main.java
@@ -1,20 +1,32 @@
 package day14;
 /*
-Twitter HTTP APIs
-------
+(i) The URL (ii) HTTP Method (iii) Request Body (iv) Response Body (v) Query Parameters
 
+Ex :
+Twitter HTTP APIs
+Suppose if there 10k tweets. There will lot of network payload
+which will result in delay
+(i) Which can fetch tweets in batches of 100.
+(ii) We also want the ability to fetch the tweets by data in ascending or descending order
+(iii) API should be configurable and you can ask for say 100 , 200 tweets
+(iv) Design the HTTP API call for the next button. Which will show the next
+100 tweets
+(v) Design the HTTP API call for the prev button. Which will show the prev
+100 tweets
+(vi) Design the HTTP API with the page number.
+HTTP API should be able to fullfill all the above requirements.
+
+------
+Return all tweets
+Ex:
 Facebook HTTP APIs
 --------------------
-1) User account creationg
+1) User account creation
 2) Follow another user
 3) Friend another user
 4) unfriend another user
 5) post
 6) likes
-
-
-
-
 
  */
 public class Main {
