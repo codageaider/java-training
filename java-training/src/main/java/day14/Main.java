3aa9744eaab7eae5bd5f09584925330fb5f11bdd
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-07 09:35:24 +0530

    random

---
+++ java-training/src/main/java/day14/Main.java
@@ -0,0 +1,21 @@
+package day14;
+/*
+Twitter HTTP APIs
+------
+
+Facebook HTTP APIs
+--------------------
+1) User account creationg
+2) Follow another user
+3) Friend another user
+4) unfriend another user
+5) post
+6) likes
+
+
+
+
+
+ */
+public class Main {
+}
