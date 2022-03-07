0fdb9be5deb76d502460f74f569423bf0c4f030a
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-07 15:38:26 +0530

    random

--- java-training/src/main/java/day14/SpringServerMain.java
+++ java-training/src/main/java/day14/SpringServerMain.java
@@ -3,7 +3,12 @@ package day14;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+/*
+Ex: Implement all the twitter and Facebook APIs only GET and POST.
 
+
+
+ */
 @SpringBootApplication
 public class SpringServerMain {
     public static void main(String[] args) {
