a6159a41ed358725611ea858ab8f2f4fde701f21
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-11 15:32:34 +0530

    random

--- java-training/src/main/java/day19/Main.java
+++ java-training/src/main/java/day19/Main.java
@@ -28,6 +28,22 @@ See which files get restored. Do a1.txt and a2.txt both appeaer or only one of t
 (xi) do rm * , git checkout master, check the contents of a1.txt and a3.txt
      post your observations
 
+1) git <-- some git commands add, diff, commit
+ Version control System
+2) github.com -> GUI for git repositories or cloud hosting of code etc.
+     (i) Access files remotely
+     (ii) Access files on different devices
+ github.com is one of the online repository services based on git
+ gitlab, bitbucket
+
+ 3) git and github are two different things
+ 4) git push
+ Push the local changes to remote github.com cloud repository
+ -) git pull
+ pull the changes from the remote github.com to local
+ 5) Create a new branch : checkout -b branchname
+ 6) git branch
+ Displays the list of all branches on your local git repository
  */
 public class Main {
 }
