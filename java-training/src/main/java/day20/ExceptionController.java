269dc50ddcf5f7d03e6ae9467001bc97a98f60e8
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-14 12:01:03 +0530

    random

--- java-training/src/main/java/day20/ExceptionController.java
+++ java-training/src/main/java/day20/ExceptionController.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(value =TweetNotFoundException.class)
     public ResponseEntity<Object> handleException(Exception exception) {
