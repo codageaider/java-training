9905d64c988fa0e880aee019225832f4b2cac4f9
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-14 09:51:07 +0530

    random

---
+++ java-training/src/main/java/day20/Main.java
@@ -0,0 +1,10 @@
+package day20;
+/*
+spring boot automatically takes care of various libraries and there versions
+(i)
+Download the jar file from spring initializr and extract it and open that
+in a new intellij project and see it should run directly
+https://start.spring.io/
+ */
+public class Main {
+}
