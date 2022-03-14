efd2c815be8cca36bf29c9cfff79252815066074
codage aider <codage@DESKTOP-NHT8PTC.localdomain>
2022-03-14 10:27:54 +0530

    random

--- java-training/src/main/java/day20/Main.java
+++ java-training/src/main/java/day20/Main.java
@@ -5,6 +5,18 @@ spring boot automatically takes care of various libraries and there versions
 Download the jar file from spring initializr and extract it and open that
 in a new intellij project and see it should run directly
 https://start.spring.io/
+
+(ii) Using properties
+All the code files are part of the application. But the properties files can
+be moved out of the code. And you can make any changes to properties file
+without effecting code.
+
+Ex:
+(i) create a custom property
+(ii) Access it in Tweet.java file
+(iii) fetch the Tweet bean from Application Context and see if the
+property value is getting initialized properly
+
  */
 public class Main {
 }
---
+++ java-training/src/main/java/day20/Tweet.java
@@ -0,0 +1,23 @@
+package day20;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tweet {
+/*
+without using properties file
+(i) Modify the code
+(ii) push the code to github
+(iii) Compile the project
+(iv) Deploy the application again
+
+
+With using properties file
+(i) Modify the properties file and restart the application
+ */
+    private String url="adasdad";
+    @Value("${logging.directory}")
+    private String loggingDirectory;
+
+}
